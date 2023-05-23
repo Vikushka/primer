e0ba8bf5581af6b0ba2bf005ae0c12de4c2eb43b
R2 <R2@MILA-SHKA>
2023-05-23 19:16:49 +0400

    Добавьте файлы проекта.

---
+++ примеры/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int[] chisla = { 1, 5, 6, 9, -10 };
+int result=0;
+
+foreach(int ch in chisla)
+{
+  if (ch > 0)
+  {
+        result++;
+        Console.WriteLine(ch);
+   }
+ 
+}
